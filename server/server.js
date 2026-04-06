9c99a01c875263b94085242a62c609b58493864b
janari01 <janari.stserbina@voco.ee>
2026-04-06 19:56:32 +0300

    Remove passport and session management setup
    Removed passport authentication and session management code.

--- server/server.js
+++ server/server.js
@@ -1,49 +1,57 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const passport = require('passport');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+// const passport = require('passport');
 
-const session = require('express-session');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const pool = require('./database');
+// const session = require('express-session');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.use(session({
-  secret: 'abc',
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(cors({
+  origin: true,
+  credentials: true
+}))
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+// app.use(session({
+//   secret: 'abc',
+//   resave: false,
+//   saveUninitialized: true
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  // Lisame vajadusel andmebaasi
-  return done(null, profile);
-}));
+// passport.deserializeUser((obj, done) => {
+//   done(null, obj);
+// });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-);
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: 'http://localhost:3000/auth/google/callback'
+// }, (accessToken, refreshToken, profile, done) => {
+//   // Lisame vajadusel andmebaasi
+//   return done(null, profile);
+// }));
+
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: ['profile', 'email'] })
+// );
 
 
-app.get('/auth/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: '/'
-  }),
-  (req, res) => {
-    res.send('Logged in as ' + req.user.displayName);
-  }
-);
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', {
+//     failureRedirect: '/'
+//   }),
+//   (req, res) => {
+//     res.send('Logged in as ' + req.user.displayName);
+//   }
+// );
 
 module.exports = app;
