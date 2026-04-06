8bdb19bd3ccbc4d93fbdd4c00e1a976d8c7d14b9
janari01 <janari.stserbina@voco.ee>
2026-04-06 17:24:37 +0000

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser')
-// const passport = require('passport');
+const passport = require('passport');
 
-// const session = require('express-session');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 app.use(cors({
   origin: true,
@@ -15,43 +15,43 @@ app.use(cors({
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// app.use(session({
-//   secret: 'abc',
-//   resave: false,
-//   saveUninitialized: true
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(session({
+  secret: 'abc',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-// passport.deserializeUser((obj, done) => {
-//   done(null, obj);
-// });
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: 'http://localhost:3000/auth/google/callback'
-// }, (accessToken, refreshToken, profile, done) => {
-//   // Lisame vajadusel andmebaasi
-//   return done(null, profile);
-// }));
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'https://ep-lorem8.vercel.app/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  // Lisame vajadusel andmebaasi
+  return done(null, profile);
+}));
 
-// app.get('/auth/google',
-//   passport.authenticate('google', { scope: ['profile', 'email'] })
-// );
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
 
-// app.get('/auth/google/callback',
-//   passport.authenticate('google', {
-//     failureRedirect: '/'
-//   }),
-//   (req, res) => {
-//     res.send('Logged in as ' + req.user.displayName);
-//   }
-// );
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/'
+  }),
+  (req, res) => {
+    res.send('Logged in as ' + req.user.displayName);
+  }
+);
 
 module.exports = app;
