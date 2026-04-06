c71c227e288466fdbd699dbcb681e07b49ae307a
janari01 <janari.stserbina@voco.ee>
2026-04-06 18:39:05 +0000

    Vercel test 4

--- server/server.js
+++ server/server.js
@@ -32,8 +32,8 @@ passport.deserializeUser((obj, done) => {
 });
 
 passport.use(new GoogleStrategy({
-  clientID: 123,
-  clientSecret: 123,
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: 'http://localhost:3000/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
   // Lisame vajadusel andmebaasi
